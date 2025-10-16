34c843a8f10804e6763b97db2e95053b112ea60e
jonathan_uber <jonathanuber121207@gmail.com>
2025-10-16 18:44:44 -0300

    feat: create entities

---
+++ src/main/java/org/maintenancesystem2/model/Supplier.java
@@ -0,0 +1,52 @@
+package org.maintenancesystem2.model;
+
+public class Supplier {
+
+    private Long id;
+    private String name;
+    private String cnpj;
+
+    public Supplier(Long id, String name, String cnpj) {
+        this.id = id;
+        this.name = name;
+        this.cnpj = cnpj;
+    }
+
+    public Supplier(String name, String cnpj) {
+        this.name = name;
+        this.cnpj = cnpj;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cnpj='" + cnpj + '\'' +
+                '}';
+    }
+}
